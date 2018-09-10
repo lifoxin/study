bdb82d2b46d717bbb390c6a3ec838b33f233ef49
foxin.li <foxin.li@inin88.com>
2018-09-10 01:51:19 +0000

    添加了上行注释部分

--- rookie/99.py
+++ rookie/99.py
@@ -2,6 +2,7 @@
 
 i,j = 1,1
 
+print("\n----------------------9x9乘法表-------------------------\n")
 for i in range(1,10):
     for j in range(1,i+1):
         print("{}x{}={}\t".format(i,j,i*j),end="")
