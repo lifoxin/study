dee7749d82263c0b0063cbf1bf2e0116338eb25c
foxin.li <foxin.li@inin88.com>
2018-09-20 06:53:39 +0000

    根据网站的爬虫协议，判断网站哪个链接是否可爬

--- spider/robots.py
+++ spider/robots.py
@@ -6,10 +6,15 @@ from urllib.request import urlopen
 
 '''
  robotparser 模块的基本用法和实例讲解，利用它我们就可以方便地判断哪些页面可以抓取
+ 本人很奇怪，为什么两个都false
 '''
-
+#网站可爬取robots协议
+roboturl='http://www.jianshu.com/robots.txt'
+#测试该网站是否可爬取
+url='http://www.jianshu.com/p/b67554025d7d'
 
 rp = RobotFileParser()
-rp.parse(urlopen('https://www.baidu.com/robots.txt').read().decode('utf-8').split('\n'))
-print(rp.can_fetch('*', '://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_8580596602608074179%22%7D&n_type=0&p_from=1'))
-print(rp.can_fetch('*', "https://www.baidu.com/search?q=python&page=1&type=collections"))
+rp.set_url(roboturl)
+rp.read()
+print(rp.can_fetch('*', url))
+print(rp.can_fetch('*', "http://www.jianshu.com/search?q=python&page=1&type=collections"))
