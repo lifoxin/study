e2a7c1ad72adc567a01b14203be64451ec39078e
foxin.li <foxin.li@inin88.com>
2018-10-16 07:10:43 +0000

    update 99.py

--- rookie/99.py
+++ rookie/99.py
@@ -5,5 +5,5 @@ i,j = 1,1
 print("\n----------------------9x9乘法表-------------------------\n")
 for i in range(1,10):
     for j in range(1,i+1):
-        print("{}x{}={}\t".format(i,j,i*j),end="")
+        print("{}x{}={}\t".format(j,i,i*j),end="")
     print()
