74ffe2fba78421101a794cd6da88631b2d01e6e3
felix <805986238@qq.com>
2019-07-31 09:35:16 +0800

    修改发送人邮箱

--- rookie/sendMail.py
+++ rookie/sendMail.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python3
 
-import smtplib  
+import smtplib
 from email.mime.text import MIMEText  
-'''
-这是一个发送邮箱的脚本测试,只是试一下
-'''
-mailto_list=[] 
-mail_host="smtp.163.com:25" 
-mail_user="18344589481@163.com" 
-mail_pass="Ab123456"
-debug_level=0       #是否开启debug
- 
-def send_mail(to_list,sub,content):  
-    me=mail_user
-    msg = MIMEText(content,_subtype='plain',_charset='utf-8')  
-    msg['Subject'] = sub  
-    msg['From'] = me  
-    msg['To'] = ";".join(to_list)  
-    try:  
-        server = smtplib.SMTP()  
-        server.set_debuglevel(debug_level)    
-        server.connect(mail_host)  
-        server.login(mail_user,mail_pass)  
-        server.sendmail(me, to_list, msg.as_string())  
-        server.close()  
-        return True  
-    except Exception as e:  
-        print('except:',e)  
-        return False  
 
+host = 'smtp.163.com'
+port = 465
+sender = '18344589481@163.com'
+pwd = 'Tx123456'
+
+def sentemail():
+	receivers = ["805986238@qq.com","381407211@qq.com"] 
+	with open("/tmp/yulu.text") as f:
+		content = f.read()
+#	msg = MIMEText(body, 'html') 
+	msg = MIMEText(content,_subtype='plain',_charset='utf-8')
+	msg['subject'] = '前程无忧运维工作' 
+	msg['from'] = sender  
+	for receiver in receivers:
+		msg['to'] = receiver  
+		s = smtplib.SMTP_SSL(host, port)  
+		# 注意！如果是使用SSL端口，这里就要改为SMTP_SSL
+		s.login(sender, pwd)  
+		s.sendmail(sender, receiver, msg.as_string().encode())
+	print ('Done.sent email success')
+			
 if __name__ == '__main__':
-    
-    mailto_list=["805986238@qq.com"]
-    sub="常见的运维工作有哪些"
-    with open('/tmp/yulu.text') as f:
-        content = f.read()
- 
-    if send_mail(mailto_list,sub,content):  
-        print("发送成功")  
-    else:  
-        print("发送失败")
+	sentemail()
