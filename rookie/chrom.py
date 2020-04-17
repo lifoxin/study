a0e008063839a954d0551ff4f302f47ac98e0eb2
felix <805986238@qq.com>
2020-04-17 16:52:50 +0800

    新增索引网站，并用默认浏览器打开

---
+++ rookie/chrom.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+import re
+import webbrowser
+
+def main():
+
+
+	file1 = open('www.txt')  #打开文件
+	str1 = file1.read()      #读取文件
+	#索引出要打开的网站
+	str2 = re.findall(r'weixin.oa.com/yard_deploy/view/ticket/EX-\d+-\d+',str1)
+
+	#打开网站
+	for url in str2:
+		url = "https://" + url 
+		webbrowser.open(url)
+		#print(url)
+
+	#关闭文件
+	file1.close()
+
+if __name__ == "__main__":
+
+	main()
